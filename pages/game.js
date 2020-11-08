5323b95b7936e8ad0a8d4c626c92aa25a6964d1d
KAM TSUN SO <alexskt123@KAMde-Air.lan>
2020-11-08 22:18:59 +0800

    beautify game

--- pages/game.js
+++ pages/game.js
@@ -5,7 +5,7 @@ import Button from 'react-bootstrap/Button'
 import Alert from 'react-bootstrap/Alert'
 import Row from 'react-bootstrap/Row'
 
-import { getWords } from '../lib/firebaseResult.js'
+import { getWords, setHistory } from '../lib/firebaseResult.js'
 import { getRandMCQuestion } from '../lib/data/dataProcess'
 
 
@@ -13,13 +13,23 @@ export default function Home({words}) {
 
   const [mc, setMC] = useState(null)
   const [nextMC, setNextMC] = useState(false)
+  const [totalCount, setTotalCount] = useState(0)
+  const [correctCount, setCorrectCount] = useState(0)
+  const [correct, setCorrect] = useState(null)
 
   const handleClick = async (question, answer) => {
-    words
+    if (words
       .filter(x=> (x['假名']===question || x['日文']===question))
       .filter(x=> x['中文']===answer)
-      .length
-      ? alert ('啱！') : alert ('唔啱！')
+      .length) {
+        setCorrectCount(correctCount + 1)
+        setCorrect('啱！')
+      }
+      else {
+        setCorrect('唔啱！')
+      }
+
+    setTotalCount(totalCount + 1)
     setNextMC(!nextMC)
   }
 
@@ -45,13 +55,19 @@ export default function Home({words}) {
     <Fragment>
       <Container style={{ minHeight: '100vh' }} className="mt-5 shadow-lg p-3 mb-5 bg-white rounded">
         <Fragment>
-          <Row>
-            <Alert variant='dark'>
+          <Row className="pl-3 mt-3">
+            <Alert variant='danger'>
               {`問題： ${mc.question}`}
+            </Alert>
+            <Alert variant='success'>
+              {`${correctCount} / ${totalCount}`}
+            </Alert>
+            <Alert variant='info'>
+              {correct}
             </Alert>
           </Row>
 
-          <Row>
+          <Row className="pl-3">
             {mc.answer.map((item, idx) => {
               return (
                 <Button onClick={() => { handleClick(mc.question, item) }}  key={idx} variant='outline-dark'>

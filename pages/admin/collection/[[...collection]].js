aefbafeda260b9b81d68f374b9139c6bf246aaf5
condomlee <eddykidness@icloud.com>
2020-11-11 19:14:14 +0800

    integrate route as collection name

--- pages/admin/collection/[[...collection]].js
+++ pages/admin/collection/[[...collection]].js
@@ -1,24 +1,35 @@
 import { Fragment, useEffect, useRef, useState } from 'react'
+import { useRouter } from 'next/router'
 import { ControlledEditor } from '@monaco-editor/react'
-import Cookies from 'js-cookie'
 
-import fire from '../../config/fire-config'
+import fire from '../../../config/fire-config'
 
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
 
 export default function Collection() {
+  const router = useRouter()
+
   const [formValue, setFormValue] = useState() // for debounce
-  const [collectionName, setCollectionName] = useState(Cookies.get('collection')) // for query
+  const [collectionName, setCollectionName] = useState() // for query
   const [collections, setCollections] = useState([]) // for result
   const [value, setValue] = useState() // for editor
 
   const valueGetter = useRef()
 
+  const getRouterPath = () => {
+    return router.asPath.replace(/\/admin\/collection\/?/g, '')
+  }
+
+  useEffect(() => {
+    const path = getRouterPath()
+    setFormValue(path)
+  }, [router])
+
   useDebounce(
     async () => {
       if (!formValue) return
-      Cookies.set('collection', formValue)
+      router.replace(`/admin/collection/${formValue}`)
       setCollectionName(formValue)
     },
     1000,
@@ -28,12 +39,15 @@ export default function Collection() {
   useEffect(() => {
     if (!collectionName) return
 
-    fire.firestore()
+    const unsub = fire.firestore()
       .collection(collectionName)
       .onSnapshot(snapShot => {
         const list = snapShot.docs.map(doc => ({ _id: doc.id, ...doc.data() }))
         setCollections(list)
       })
+
+    //must return to cleanup
+    return unsub
   }, [collectionName])
 
   useEffect(() => {
@@ -56,7 +70,7 @@ export default function Collection() {
           <InputGroup.Prepend>
             <InputGroup.Text>Firestore/</InputGroup.Text>
           </InputGroup.Prepend>
-          <FormControl id="collectionName" placeholder="Collection Name" onKeyUp={handleCollectionChange} defaultValue={Cookies.get('collection')} />
+          <FormControl id="collectionName" placeholder="Collection Name" onKeyUp={handleCollectionChange} defaultValue={getRouterPath()} />
         </InputGroup>
         <h1>Editor</h1>
         <ControlledEditor
